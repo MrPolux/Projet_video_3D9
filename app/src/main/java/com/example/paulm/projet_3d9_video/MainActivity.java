9ebc65c64a6acba77cb69423bb10eec9d4ba154e
MrPolux <34023451+MrPolux@users.noreply.github.com>
2017-12-11 18:04:01 +0100

    Second commit

--- app/src/main/java/com/example/paulm/projet_3d9_video/MainActivity.java
+++ app/src/main/java/com/example/paulm/projet_3d9_video/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.paulm.projet_3d9_video;
 
+import android.media.MediaPlayer;
+import android.media.PlaybackParams;
 import android.support.v7.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,16 +13,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
+
+import java.io.IOException;
 import java.net.URI;
+import java.sql.Time;
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity  {
 
-    private Button mPrevButton, mPlayButton, mPauseButton, mNextButton;
+    private Button mPrevButton, mPlayButton, mPauseButton, mNextButton, mBackwardButton, mForwardButton;
     private VideoView mVideoView;
     private  ArrayList<android.net.Uri> arrayList;
     private int index ;
+    private int seekForwardTime = 3 * 1000;
+    private int seekBackwardTime = 3 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +37,16 @@ public class MainActivity extends AppCompatActivity  {
         mPlayButton = (Button) findViewById(R.id.PlayButton);
         mPauseButton = (Button) findViewById(R.id.PauseButton);
         mNextButton = (Button) findViewById(R.id.NextButton);
+        mForwardButton = (Button) findViewById(R.id.ForwardButton);
+        mBackwardButton = (Button) findViewById(R.id.BackwardButton);
         mVideoView = (VideoView) findViewById(R.id.videoView);
         arrayList = new ArrayList<>();
 
 
 
-        arrayList.add(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.chien ));
+        arrayList.add(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.bojack));
         arrayList.add(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.blabla ));
-        arrayList.add(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.chatte ));
+        arrayList.add(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.morty ));
 
         setupMedia();
         setupListeners();
@@ -96,16 +105,55 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
 
+        mBackwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rewindSong();
+            }
+        });
 
+        mForwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                forwardSong();
+            }
+        });
 
     }
 
 
 
+    public void forwardSong() {
+        if (mVideoView != null) {
+            int currentPosition = mVideoView.getCurrentPosition();
+            if (currentPosition + seekForwardTime <= mVideoView.getDuration()) {
+                mVideoView.seekTo(currentPosition + seekForwardTime);
+            } else {
+                mVideoView.seekTo(mVideoView.getDuration());
+            }
 
+            mVideoView.start();//start your video.
+        }
+    }
 
-
-
+    public void rewindSong() {
+        if (mVideoView != null) {
+            int currentPosition = mVideoView.getCurrentPosition();
+            if (currentPosition - seekBackwardTime >= 0) {
+                mVideoView.seekTo(currentPosition - seekBackwardTime);
+            } else {
+                mVideoView.seekTo(0);
+            }
+        }
+    }
 
 }
 
+
+    /*public void test(VideoView videoView, final float vitesse){
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                PlaybackParams myPlayBackParams = new PlaybackParams();
+                myPlayBackParams.setSpeed(vitesse);
+                mp.setPlaybackParams(myPlayBackParams);*/
